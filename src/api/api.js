e1c59f1582fd454764f70a8c7418dde47a687fc1
Fablaney <fabrmoto@gmail.com>
2022-09-16 16:32:48 +0200

    call API

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,39 @@
 import axios from "axios"
 
 /**
- * 
+ * return user mains datas
  * @param {string} id 
  * @returns {promise}
  */
-const fetchActivity = (id) => {
+const fetchUsermainDatas = (id) => {
+    return axios.get('http://localhost:3000/user/'+ id)
+}
+
+/**
+ * return user activity
+ * @param {string} id 
+ * @returns {promise}
+ */
+ const fetchActivity = (id) => {
     return axios.get('http://localhost:3000/user/'+ id +'/activity')
 }
 
+/**
+ * return user average sessions
+ * @param {string} id 
+ * @returns {promise}
+ */
+ const fetchAverageSessions = (id) => {
+    return axios.get('http://localhost:3000/user/'+ id +'/average-sessions')
+}
 
-export {fetchActivity}
+/**
+ * return user performances
+ * @param {string} id 
+ * @returns {promise}
+ */
+ const fetchPerformance = (id) => {
+    return axios.get('http://localhost:3000/user/'+ id +'/performance')
+}
+
+export { fetchUsermainDatas, fetchActivity, fetchAverageSessions, fetchPerformance }
